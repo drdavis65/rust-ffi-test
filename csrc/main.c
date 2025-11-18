26dccc08faf8b4ccd4bc798e6fb3cd2ce6662917
ddavis65 <ddavis65@node0.ddavis65-280196.c-rust-perf-pg0.utah.cloudlab.us>
2025-11-18 02:59:12 -0700

    benchmarking noalias test functions

--- csrc/main.c
+++ csrc/main.c
@@ -1,19 +1,60 @@
+#define _GNU_SOURCE
 #include "mylib.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <stdint.h>
+
+static inline uint64_t ns_since_epoch(void) {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts); // or CLOCK_MONOTONIC_RAW
+    return (uint64_t)ts.tv_sec * 1000000000ull + ts.tv_nsec;
+}
 
 int main() {
+
     Foo a = {5, 1.5};
-    Foo b = {2, 3.0};
+    Foo b = {2, 3.0};   
 
-    printf("a: id=%d value=%lf, b: id=%d value=%lf\n", a.id, a.value, b.id, b.value);
+    // Check initial values
+//    printf("a: id=%d value=%lf, b: id=%d value=%lf\n", a.id, a.value, b.id, b.value);
 
-    process_restricted(&a, &b);
+    // Benchmark "process" function
+    uint64_t start = ns_since_epoch();
 
-    printf("a: id=%d value=%lf, b: id=%d value=%lf\n", a.id, a.value, b.id, b.value);
+    int i;
+    for(i = 0; i < 1000; i++) {
+        process(&a, &b);
+    }
 
-    size_t n = 16;
+    uint64_t end = ns_since_epoch() - start;
+
+    // Check ending values to guarantee correctness
+//    printf("a: id=%d value=%lf, b: id=%d value=%lf\n", a.id, a.value, b.id, b.value);
+    printf("Process time(ms): %f\n", end / 1e6);
+
+
+    /* Initialize Foo structs same as before */
+
+    Foo c = {5, 1.5};
+    Foo d = {2, 3.0};   
+
+//    printf("c: id=%d value=%lf, d: id=%d value=%lf\n", c.id, c.value, d.id, d.value);
+
+    // Benchmark noalias "process" function
+    start = ns_since_epoch();
+
+    for(i = 0; i < 1000; i++) {
+        process_restricted(&c, &d);
+    }
+
+    end = ns_since_epoch() - start;
+
+//    printf("c: id=%d value=%lf, d: id=%d value=%lf\n", c.id, c.value, d.id, d.value);
+    printf("Process_restricted time(ms): %f\n", end / 1e6);
+
+    size_t n = 32;
 
     Pt* dst = (Pt*)malloc(n * sizeof(*dst));
     
@@ -28,11 +69,40 @@ int main() {
 
     float alpha = 0.5f;
 
-    axpy_pts(n, dst, src, alpha);
 
+    // Benchmark axpy_pts
+    start = ns_since_epoch();
+
+    for(i = 0; i < 1000; i++) {
+        axpy_pts(n, dst, src, alpha);
+    }
+    
+    end = ns_since_epoch() - start;
+//    printf("First few values of dst: x[0]=%f y[0]=%f x[1]=%f y[1]=%f x[2]=%f y[2]=%f\n",
+//            dst[0].x, dst[0].y, dst[1].x, dst[1].y, dst[2].x, dst[2].y);
+
+    printf("Axpy_pts time(ms): %f\n", end / 1e6);
+    memset(src, 0, n * sizeof(*src));
     memset(dst, 0, n * sizeof(*dst));
 
-    axpy_pts_restricted(n, dst, src, alpha);
+    for (size_t i = 0; i < n; i++) {
+        src[i].x = (float)i;
+        src[i].y = 2.0f * (float)i;
+    }
+
+    // Benchmark noalias axpy_pts    
+    start = ns_since_epoch();
+
+    for(i = 0; i < 1000; i++) {
+        axpy_pts_restricted(n, dst, src, alpha);
+    }
+    
+    end = ns_since_epoch() - start;
+
+//    printf("First few values of dst: x[0]=%f y[0]=%f x[1]=%f y[1]=%f x[2]=%f y[2]=%f\n",
+//            dst[0].x, dst[0].y, dst[1].x, dst[1].y, dst[2].x, dst[2].y);
+
+    printf("Axpy_pts_restricted time(ms): %f\n", end / 1e6);
 
     free(dst);
     free(src);
